3a28749e473fcab7cfbb831b912d081719195bcf
bilal-23 <mansuribilal101@gmail.com>
2021-06-25 20:32:58 +0530

    final commit

--- src/components/Media.js
+++ src/components/Media.js
@@ -1,24 +1,12 @@
 import React, { useRef, useState, useEffect } from 'react'
 
 const Media = ({ url, active, x, y }) => {
-    const targetRef = useRef();
-    const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
-    useEffect(() => {
-        if (targetRef.current) {
-            console.log('workking')
-            setDimensions({
-                width: targetRef.current.offsetWidth,
-                height: targetRef.current.offsetHeight
-            });
-        }
-    }, []);
-    console.log("dimenstions", dimensions)
+
     return (
         <img src={url}
-            ref={targetRef}
             className={`${active && 'is-active'}`}
             alt="sample data"
-            style={{ transform: 'translate(' + (x - (dimensions.width / 2)) + 'px,' + (y - (dimensions.height / 2)) + 'px)' }}
+            style={{ transform: 'translate(' + (x - 164.5) + 'px,' + (y - 164.5) + 'px)' }}
         />
     )
 
@@ -27,4 +15,5 @@ const Media = ({ url, active, x, y }) => {
 export default Media;
 
 
-//in transform we subtract the x and y by half of height and width to center
+//in transform we subtract the x and y by half of height and width to center it.
+//Custom hook wasnt working in deployment so i hardcoded the value
