7d41f8dbf22010617df360dae8dc6a73eb51a5c1
bilal-23 <mansuribilal101@gmail.com>
2021-06-25 20:14:59 +0530

    fixed image hover

--- src/components/Media.js
+++ src/components/Media.js
@@ -1,35 +1,23 @@
-import React, { useCallback, useState, useLayoutEffect } from 'react'
-
-const getDimensionsObject = (object) => {
-    const rect = object.getBoundingClientRect();
-    return {
-        width: rect.width,
-        height: rect.height
-    };
-}
-const useSize = () => {
-    const [dimensions, setDimensions] = useState({});
-    const [node, setNode] = useState(null);
-    const ref = useCallback((node) => {
-        setNode(node)
-    }, [])
-    useLayoutEffect(() => {
-        if (node) {
-            const measure = () => setDimensions(getDimensionsObject(node));
-            measure();
-        }
-    }, [node])
-    return [ref, dimensions]
-}
+import React, { useRef, useState, useEffect } from 'react'
 
 const Media = ({ url, active, x, y }) => {
-    const [ref, { width, height }] = useSize();
+    const targetRef = useRef();
+    const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+    useEffect(() => {
+        if (targetRef.current) {
+            setDimensions({
+                width: targetRef.current.offsetWidth,
+                height: targetRef.current.offsetHeight
+            });
+        }
+    }, []);
+    console.log(dimensions)
     return (
         <img src={url}
-            ref={ref}
+            ref={targetRef}
             className={`${active && 'is-active'}`}
             alt="sample data"
-            style={{ transform: 'translate(' + (x - (width / 2)) + 'px,' + (y - (height / 2)) + 'px)' }}
+            style={{ transform: 'translate(' + (x - (dimensions.width / 2)) + 'px,' + (y - (dimensions.height / 2)) + 'px)' }}
         />
     )
 
